214cf1060eb5530d738e53d6d37a249cdebcc5e4
Demaris Ashmore <demiashmore@yahoo.com>
2022-09-11 17:07:53 -0600

    commit tree 8fcf8e83de8a8dd5da050458e8c310d8c5cff647

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+import ReactDOM from 'react-dom';
+
+function EyesOnMe() {
+    function focusEvent () {
+        console.log('Good!')
+    }
+    function blurEvent() {
+        console.log('Hey! Eyes on me!')
+    }
+
+    return (
+        <button onFocus={focusEvent} onBlur={blurEvent}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,16 @@
 // Code Keypad Component Here
+import React from 'react'
+import ReactDOM from 'react-dom';
 
 function Keypad (){
+    function handleChange() {
+        console.log('Entering password...')
+    }
+
     return (
-        <div></div>
+        <div>
+            <input onChange={handleChange} type="password" />
+        </div>
     )
 }
 
